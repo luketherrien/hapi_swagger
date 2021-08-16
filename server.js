2d4ec025d1b7f0107093396645235c58db7a0c02
Luke Therrien <luke.f.therrien@gmail.com>
2021-08-16 15:21:34 -0400

    :sparkles: Added Swagger
    Added Swagger

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi')
+const Inert = require('@hapi/inert')
+const Vision = require('@hapi/vision')
+const Swagger = require('hapi-swagger')
+const Pack = require('./package')
 
 const start = async () => {
     const server = getServer()
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: Swagger,
+            options: getSwaggerOptions()
+        }
+    ])
     await server.start()
     console.log('Server started at http://localhost:3000')
+    console.log('API Documentation served at http://localhost:3000/documentation')
 }
 
 const getServer = () => {
@@ -15,12 +28,26 @@ const getServer = () => {
     })
     server.route({
         method: 'GET',
+        options: {
+            tags: [
+                'api' // required by Swagger
+            ]
+        },
         path: '/',
         handler: (__request, __toolkit) => {
             return 'Hello World!'
         }
     })
     return server
+}
+
+const getSwaggerOptions = () => {
+    return {
+        info: {
+            title: 'API Documentation',
+            version: Pack.version
+        }
+    }
 }
 
 process.on('unhandledRejection', (error) => {
